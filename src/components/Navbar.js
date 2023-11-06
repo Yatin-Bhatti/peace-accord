727588e4db1d6a24de5206bba8f40f30b63866c8
Yatin-Bhatti <yatin@perceptsystems.com>
2023-11-06 16:21:22 +0530

    made it better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 function Navbar({Login_email,login_password}) {
 const[loginHeading,setLoginHeading]=useState("LOG IN");
 const[clickable,setClickable]=useState(true)
+const[burger_class,setBurgerClass]=useState("burger-bar unclicked");
 useEffect(()=>{
 if(login_password!==""&&Login_email!==""){
   setLoginHeading("Yatin : 3")
@@ -14,19 +15,39 @@ if(login_password!==""&&Login_email!==""){
 },[login_password,Login_email])
 
     const[menuOpen,setMenuOpen]=useState(false)
+
+    const updateMenu=()=>{
+      if(!menuOpen){
+        setBurgerClass("burger-bar clicked");
+      }
+      else{
+        setBurgerClass("burger-bar unclicked");
+      }
+      setMenuOpen(!menuOpen)
+    }
   return (
     <nav >
-      
+      {/* <div style={{display:"flex",flexDirection:"row",justifyContent:'space-between',alignItems:"center"}}> */}
+      <div className='iconAndBurger' >
     <Link to="/" onClick={()=>setMenuOpen(false)} className="logo" style={{display:"flex",flexDirection:"row"}}>
       <div className="icon">
       <Union/>
       </div>
       PEACE ACCORD</Link>
-    <div className="menu" onClick={()=>setMenuOpen(!menuOpen)}>
+    {/* <div className="menu" onClick={()=>setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
         <span></span>
+    </div> */}
+    <div className="contain">
+    <div className="burger_menu" onClick={updateMenu}>
+      <div className={burger_class} onClick={updateMenu}></div>
+      <div className={burger_class} onClick={updateMenu}></div>
+      <div className={burger_class} onClick={updateMenu}></div>
+      </div>
     </div>
+    </div>
+
         <ul className={menuOpen?"open":""}>
         <li onClick={()=>setMenuOpen(!menuOpen)} >
           <NavLink to="/review">REVIEW</NavLink>
