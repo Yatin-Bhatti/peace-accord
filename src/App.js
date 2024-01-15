35c20045128f41e0d480b9e704eb86fb56c00f86
Yatin-Bhatti <yatin@perceptsystems.com>
2024-01-15 17:44:47 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function App() {
   const [loading,setLoading]=useState(true);
   const token=useSelector((state)=>state.loginProcess.token)
   console.log(token)
+  console.log(process.env)
   const location=useLocation();
   const dispatch=useDispatch();
  const navigate=useNavigate();
